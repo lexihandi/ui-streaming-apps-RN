37aada74953ba1242350eeda0c73d51a278ed093
lexyhandi <lexihandi@gmail.com>
2021-01-29 12:57:58 +0700

    update components

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Home} from './pages';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+import Home from './home';
+
+export {Home};
