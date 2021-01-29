47637d4c5778bbd73aeac0661cac2e1d4a6a46fe
lexyhandi <lexihandi@gmail.com>
2021-01-29 18:58:21 +0700

    add utils

---
+++ src/utils/colors/index.js
@@ -0,0 +1,17 @@
+const mainColors = {
+  white: '#fff',
+  yellow: '#FFB800',
+  black: '#20242B',
+  grey: '#AFAFAF',
+  gradient: '#C4C4C4',
+};
+
+export const colors = {
+  background: mainColors.white,
+  yellow: mainColors.yellow,
+  text: {
+    primary: mainColors.black,
+    secondary: mainColors.grey,
+  },
+  gradient: mainColors.gradient,
+};
---
+++ src/utils/fonts/index.js
@@ -0,0 +1,8 @@
+export const fonts = {
+  primary: {
+    400: 'Poppins-Regular',
+    500: 'Poppins-Medium',
+    600: 'Poppins-SemiBold',
+    700: 'Poppins-Bold',
+  },
+};
